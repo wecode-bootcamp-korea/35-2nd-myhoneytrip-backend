923f6217327e17e4fec6f3349f35e9bae325a802
Ahnsang0915 <tmqjf4921@gmail.com>
2022-08-05 19:27:32 +0900

    예약 API

--- myhoneytrip/urls.py
+++ myhoneytrip/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('users', include('users.urls')),
-    path('flights', include('flights.urls'))
+    path('flights', include('flights.urls')),
+    path('bookings', include('bookings.urls'))
 ]
