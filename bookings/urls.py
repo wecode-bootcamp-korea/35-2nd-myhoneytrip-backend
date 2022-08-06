ca8913203b12e2405542997d997542685db9c95b
Ahnsang0915 <tmqjf4921@gmail.com>
2022-08-06 22:22:39 +0900

    Add canceling

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,7 +1,8 @@
 from django.urls    import path
 
-from bookings.views import BookingView
+from bookings.views import BookingView, MyBookingListView
 
 urlpatterns = [
-    path('', BookingView.as_view())
+    path('', BookingView.as_view()),
+    path('/mytrip',MyBookingListView.as_view())
 ]
