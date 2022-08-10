ba1d9d9909fa86a0c3f54be8ee3452872709b393
xoxowo <1qq11212@gmail.com>
2022-08-10 17:46:51 +0900

    conflict 해결

--- flights/tests.py
+++ flights/tests.py
@@ -1,3 +1,149 @@
-from django.test import TestCase
+from django.test    import TestCase, Client
 
-# Create your tests here.
+from flights.models import FlightDetail, FlightRoute, Airline, Location, Airplane
+
+class FlightListViewTest(TestCase):
+    def setUp(self):
+        client = Client()
+
+        hawaii = Location.objects.create(
+            id           = 1,
+            name         = 'Hawaii',
+            korean_name  = '하와이',
+            airport_code = 'HNL',
+            airport_name = 'Honolulu'
+        )
+
+        seoul = Location.objects.create(
+            id           = 2,
+            name         = 'Seoul',
+            korean_name  = '서울',
+            airport_code = 'SEL',
+            airport_name = 'Incheon'
+        )
+
+        Airline.objects.create(
+            id       = 1,
+            name     = 'HoneyAirline',
+            logo_url = '1'
+        )
+
+        Airline.objects.create(
+            id       = 2,
+            name     = 'MoonAirline',
+            logo_url = '2'
+        )
+
+        Airplane.objects.create(
+            id              = 1,
+            name            = 'UPG',
+            airplane_code   = 35,
+            number_of_seats = 100,
+            airline_id      = 1
+        )
+
+        Airplane.objects.create(
+            id              = 2,
+            name            = 'HYJ',
+            airplane_code   = 35,
+            number_of_seats = 100,
+            airline_id      = 2
+        )
+
+        FlightRoute.objects.create(
+            id          = 1,
+            code        = 'HN072',
+            airplane_id = 1,
+            departure   = seoul,
+            destination = hawaii
+        )
+
+        FlightRoute.objects.create(
+            id          = 2,
+            code        = 'MO071',
+            airplane_id = 2,
+            departure   = seoul,
+            destination = hawaii
+        )
+
+        FlightDetail.objects.create(
+            id              = 1,
+            departure_time  = '2022-08-11 01:30',
+            arrival_time    = '2022-08-11 04:30',
+            remaining_seats = 50,
+            reserved_seats  = 50,
+            price           = 2222222.00,
+            flight_route_id = 1 
+        )
+        
+        FlightDetail.objects.create(
+            id              = 2,
+            departure_time  = '2022-08-11 01:00',
+            arrival_time    = '2022-08-11 04:00',
+            remaining_seats = 50,
+            reserved_seats  = 50,
+            price           = 2222222.00,
+            flight_route_id = 2 
+        )
+
+    def tearDown(self):
+        Location.objects.all().delete()
+        Airline.objects.all().delete()
+        Airplane.objects.all().delete()
+        FlightDetail.objects.all().delete()
+        FlightRoute.objects.all().delete()
+
+    def test_all_flight_list(self):
+        client   = Client()
+        response = client.get('/flights?departure_name=Seoul&destination_name=Hawaii&departure_date=11&passenger=1&airline=HoneyAirline&airline=MoonAirline')
+
+        self.assertEqual(response.json(),
+            {   
+                "departure_list": [
+                    {
+                        "flight_detail_id"           : 1,
+                        "flight_route_code"          : "HN072",
+                        "departure_location"         : "Seoul",
+                        "departure_location_korean"  : "서울",
+                        "departure_airport_code"     : "SEL",
+                        "destination_location"       : "Hawaii",
+                        "destination_location_korean": "하와이",
+                        "destination_airport_code"   : "HNL",
+                        "month"                      : 8,
+                        "date"                       : 11,
+                        "day"                        : 4,
+                        "airline"                    : "HoneyAirline",
+                        "airline_url"                : "1",
+                        "departure_time"             : "01:30",
+                        "arrivel_time"               : "04:30",
+                        "flight_time"                : "3:00",
+                        "price"                      : "2222222.00",
+                        "remaining_seats"            : 50,
+                        "pssengers"                  : "1"
+                    },
+                    {
+                        "flight_detail_id"           : 2,
+                        "flight_route_code"          : "MO071",
+                        "departure_location"         : "Seoul",
+                        "departure_location_korean"  : "서울",
+                        "departure_airport_code"     : "SEL",
+                        "destination_location"       : "Hawaii",
+                        "destination_location_korean": "하와이",
+                        "destination_airport_code"   : "HNL",
+                        "month"                      : 8,
+                        "date"                       : 11,
+                        "day"                        : 4,
+                        "airline"                    : "MoonAirline",
+                        "airline_url"                : "2",
+                        "departure_time"             : "01:00",
+                        "arrivel_time"               : "04:00",
+                        "flight_time"                : "3:00",
+                        "price"                      : "2222222.00",
+                        "remaining_seats"            : 50,
+                        "pssengers"                  : "1"
+                    }
+                ],
+                "return_list" : []
+            }
+        )
+        self.assertEqual(response.status_code, 200)
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,87 @@
-from django.shortcuts import render
+from django.views     import View
+from django.http      import JsonResponse
 
-# Create your views here.
+from flights.models   import FlightDetail, FlightRoute, Location
+
+class FlightListView(View):
+    def get(self, request):
+        try:
+            data             = request.GET
+            departure_name   = data['departure_name']
+            destination_name = data['destination_name']
+            departure_date   = data['departure_date']
+            return_date      = request.GET.get('return_date', 0)
+            passengers       = data['passenger']
+            airlines         = request.GET.getlist('airline')
+            sort_type        = request.GET.get('sort_by','lowest_fare')
+            offset           = int(request.GET.get('offset', 0))
+            limit            = int(request.GET.get('limit', 5))
+
+            sort_options = {
+                'lowest_fare' : 'price',
+                'fastest_time': 'departure_time'
+            }
+            departure_options = FlightDetail.objects.select_related('flight_route') \
+                                .filter(departure_time__day=departure_date, \
+                                        flight_route__departure__name=departure_name, \
+                                        flight_route__destination__name=destination_name, \
+                                        remaining_seats__gte=passengers, \
+                                        flight_route__airplane__airline__name__in=airlines).order_by(sort_options[sort_type])
+
+            return_options = FlightDetail.objects.select_related('flight_route') \
+                            .filter(departure_time__day=return_date, \
+                                    flight_route__departure__name=destination_name, \
+                                    flight_route__destination__name=departure_name, \
+                                    remaining_seats__gte=passengers, 
+                                    flight_route__airplane__airline__name__in=airlines).order_by(sort_options[sort_type])
+
+            get_flight_time = lambda x : round(x.seconds/3600)
+
+            departure_info = [{
+                'flight_detail_id'           : departure_option.id,
+                'flight_route_code'          : departure_option.flight_route.code,
+                'departure_location'         : departure_option.flight_route.departure.name,
+                'departure_location_korean'  : departure_option.flight_route.departure.korean_name,
+                'departure_airport_code'     : departure_option.flight_route.departure.airport_code,
+                'destination_location'       : departure_option.flight_route.destination.name,
+                'destination_location_korean': departure_option.flight_route.destination.korean_name,
+                'destination_airport_code'   : departure_option.flight_route.destination.airport_code,
+                'month'                      : departure_option.departure_time.month,
+                'date'                       : departure_option.departure_time.day,
+                'day'                        : departure_option.departure_time.isoweekday(),
+                'airline'                    : departure_option.flight_route.airplane.airline.name,
+                'airline_url'                : departure_option.flight_route.airplane.airline.logo_url,
+                'departure_time'             : departure_option.departure_time.strftime('%H:%M'),
+                'arrival_time'               : departure_option.arrival_time.strftime('%H:%M'),
+                'flight_time'                : get_flight_time(departure_option.departure_time-departure_option.arrival_time),
+                'price'                      : departure_option.price,
+                'remaining_seats'            : departure_option.remaining_seats,
+                'passengers'                 : passengers
+                } for departure_option in departure_options[offset:offset+limit]]
+
+            return_info = [{
+                'flight_detail_id'           : return_option.id,
+                'flight_route_code'          : return_option.flight_route.code,
+                'departure_location'         : return_option.flight_route.departure.name,
+                'departure_location_korean'  : return_option.flight_route.departure.korean_name,
+                'departure_airport_code'     : return_option.flight_route.departure.airport_code,
+                'destination_location'       : return_option.flight_route.destination.name,
+                'destination_location_korean': return_option.flight_route.destination.korean_name,
+                'destination_airport_code'   : return_option.flight_route.destination.airport_code,
+                'month'                      : return_option.departure_time.month,
+                'date'                       : return_option.departure_time.day,
+                'day'                        : return_option.departure_time.isoweekday(),
+                'airline'                    : return_option.flight_route.airplane.airline.name,
+                'airline_url'                : return_option.flight_route.airplane.airline.logo_url,
+                'departure_time'             : return_option.departure_time.strftime('%H:%M'),
+                'arrival_time'               : return_option.arrival_time.strftime('%H:%M'),
+                'flight_time'                : get_flight_time(return_option.departure_time - return_option.arrival_time),
+                'price'                      : return_option.price,
+                'remaining_seats'            : return_option.remaining_seats,
+                'passengers'                 : passengers
+                } for return_option in return_options[offset:offset+limit]]
+
+            return JsonResponse({'departure_list': departure_info,'return_list':return_info} , status=200)
+                
+        except KeyError:
+            return JsonResponse({'message':'Key_Error'}, status=400)
